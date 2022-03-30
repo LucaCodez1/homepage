08b8f454dc1edacd06ee12e2eb0804d5c40a9f1d
BossFreind1234 <vincentredtube@gmail.com>
2022-03-30 18:27:11 -0400

    added more stuff to the footer

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ function jobRedirect() {
     window.location.href = "https://jobs.tacobyte.dev"
 }
 
+function discordRedirect() {
+    window.location.href = "https://discord.gg/acPGzacTF5 "
+}
+
+function githubRedirect() {
+    window.location.href = "https://github.com/Team-Tacobyte"
+}
+
+function betaSignup() {
+    window.location.href = "/pages/signup.html"
+}
